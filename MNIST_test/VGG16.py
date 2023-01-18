528d4adae025184090d8a63790e20793cadcee19
kbylander <98012682+kbylander@users.noreply.github.com>
2023-01-18 21:08:27 +0100

    update

--- MNIST_test/VGG16.py
+++ MNIST_test/VGG16.py
@@ -5,35 +5,41 @@ import torch
 import torch.nn as nn
 from torchvision import datasets
 from torchvision import transforms
+from torchvision.models import vgg16, VGG16_Weights
 import matplotlib.pyplot as plt
 from torch.utils.data.sampler import SubsetRandomSampler
 
 device = torch.device("cpu")
 
-#load and transform data
+#load and transform data for training, validation and test set.  
 def load_dataset(test=False,validation_size=0):
     #transforms images(rezises and crops, transform to tensor and then normalize based on mean and standard deviation)
-    trf=transforms.Compose([transforms.Resize(32),transforms.CenterCrop(32),transforms.ToTensor(),transforms.Normalize(mean=0.1285,std=0.2864)])
+    trf=transforms.Compose([transforms.Grayscale(3),transforms.Resize(32),transforms.CenterCrop(32),transforms.ToTensor(),transforms.Normalize(mean=0.1285,std=0.2864)])
     if test:
         data_name=datasets.MNIST(root='./data',download=True,train=False,transform = trf)
-        return torch.utils.data.DataLoader(data_name, batch_size=len(data_name), shuffle=True)
+        return torch.utils.data.DataLoader(data_name, batch_size=50, shuffle=True)
     
     data_name=datasets.MNIST(root='./data',download=True,train=True,transform = trf)
-    
+
+    len_data=1000
     #create list of ints the length of dataset
-    indices = list(range(len(data_name)))
+    indices = list(range(len_data))
 
     #Calculate the number of instances in the validation set
-    split_ints=int(np.floor(validation_size*len(data_name)))
+    split_ints=int(np.floor(validation_size*len_data))
     
     #random shuffles and samples the indices to train/validation samples and then returns the respective data
     np.random.shuffle(indices)
     train_samples, valid_samples= SubsetRandomSampler(indices[split_ints:]), SubsetRandomSampler(indices[:split_ints])
-    return torch.utils.data.DataLoader(data_name, batch_size=60000,sampler=train_samples), torch.utils.data.DataLoader(data_name, batch_size=60000,sampler=valid_samples)
+    return torch.utils.data.DataLoader(data_name, batch_size=1,sampler=train_samples), torch.utils.data.DataLoader(data_name, batch_size=1,sampler=valid_samples)
 
 
 test_data=load_dataset(test=True)
 train_data,valid_data=load_dataset(validation_size=0.2)
+print(len(train_data))
+print(len(valid_data))
+print(len(test_data))
+
 #calculate mean and std for dataset
 def calc_mean_std(dataset):
     images, labels=next(iter(dataset))
@@ -42,14 +48,74 @@ def calc_mean_std(dataset):
 #printed the values, which were inserted into row 20 (transforms.Normalize)
 #print(calc_mean_std(data_loader))
 
-
+#plots a random image from the (test/training/validation data)
 def plot_image(dataset):
     images, labels = next(iter(dataset))
-    print(len(images))
     images = images.numpy()
     images = images.transpose(0,2,3,1)
     plt.imshow(images[100])
     return plt.show()
 
-#plots a random image from the dataset
 #plot_image(test_data)
+
+no_classes=10
+no_epochs = 5
+batch_size = 1000
+learning_rate = 0.001
+
+#model with number of classes = 10 and hyperparameters:
+model = vgg16(10,weights=VGG16_Weights.DEFAULT).to(device)
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+
+# Train the model
+size_training_set = len(train_data)
+
+for epoch in range(0,no_epochs):
+    for i,(images,labels) in enumerate(train_data):
+        images=images.to(device)
+        images=images.to(device)
+
+        outputs=model(images)
+        loss=loss_function(outputs,labels)
+
+        #optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f'Epoch {epoch} of {no_epochs} with loss:{loss.item()}')
+    
+    with torch.no_grad():
+        correct = 0 
+        total = 0
+        for images, labels in valid_data:
+            images=images.to(device)
+            labels=labels.to(device)
+            outputs = model(images)
+
+            #returns the most likely class predicted
+            _, predicted = torch.max(outputs.data,1)
+
+            #calculates accuracy
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            del images, labels, outputs
+            print(f'validation accuracy of network {correct/total}')
+
+
+with torch.no_grad():
+    correct = 0 
+    total = 0
+    for images, labels in test_data:
+        images=images.to(device)
+        labels=labels.to(device)
+        outputs = model(images)
+
+        #returns the most likely class predicted
+        _, predicted = torch.max(outputs.data,1)
+
+        #calculates accuracy
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        del images, labels, outputs
+        print(f'test accuracy of network {correct/total}')
+
---
+++ MNIST_test/test.py
@@ -0,0 +1,81 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from torchvision import transforms
+from torch.autograd import Variable
+from torchvision.models import vgg16, VGG16_Weights
+import matplotlib.pyplot as plt
+from torch.utils.data.sampler import SubsetRandomSampler
+from torchsummary import summary                     
+
+
+device = torch.device("cpu")
+
+#transforms images(rezises and crops, transform to tensor)
+test_data=datasets.MNIST(root='./data',download=True,train=False,transform = transforms.Compose([transforms.Grayscale(3),transforms.CenterCrop(32),transforms.Resize(32),transforms.ToTensor()]))
+train_data=datasets.MNIST(root='./data',train=True,transform = transforms.Compose([transforms.Grayscale(3),transforms.CenterCrop(32),transforms.Resize(32),transforms.ToTensor()]))
+
+"""
+figure = plt.figure(figsize=(10, 8))
+cols, rows = 5, 5
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(train_data), size=(1,)).item()
+    img, label = train_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(label)
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
+plt.show()
+"""
+
+load_data = {'test' : torch.utils.data.DataLoader(test_data, batch_size=64, shuffle=True),
+ 'train' : torch.utils.data.DataLoader(train_data,batch_size=64,shuffle=True)
+}
+
+model = vgg16(10)
+summary(model)                    #LINE 2
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
+
+no_epochs = 1
+
+def train(num_epochs, model, data):
+    model.train()
+
+    no_steps = len(data["train"])
+    for epoch in range(num_epochs):
+        for i, (images,labels) in enumerate(data["train"]):
+            output= model(images)
+            loss = loss_function(output,labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (i+1) % 10 == 0:
+                print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                       .format(epoch + 1, num_epochs, i + 1, no_steps, loss.item()))
+            if (i+1 == 10): break
+        pass
+
+    pass
+
+def test(data,model):
+    #setting drouput and normalization layers to evaluation mode
+    model.eval()
+    acc=0
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for i,(images, labels) in enumerate(data["test"]):
+            test_output = model(images)
+            train_acc = torch.sum(test_output == labels)
+            print(len(labels))
+            acc=train_acc/len(labels)
+            print('test accuracy: %.2f' % acc)
+            if (i+1) == 100: break
+        print('final test accuracy: %.2f' % acc)
+
+
+train(no_epochs,model,load_data)
+test(load_data,model)
