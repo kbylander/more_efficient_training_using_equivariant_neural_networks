977ab061a16f644e9149d08dee33c85a72d844e0
kalle <98012682+kbylander@users.noreply.github.com>
2023-05-16 10:52:18 +0200

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+#insert your own path to respective data set partition
+
+class TEM_dataset():
+    def __init__(self, mode, transform=None):
+        assert mode in ['train','validation', 'test','augmented_train']
+
+        FILE_PATH='/mimer/NOBACKUP/groups/naiss2023-22-69/exjobb/data_backup/'
+        
+        if mode == "augmented_train":
+            data = np.load(FILE_PATH+'TEM_augmented_train_compressed.npy',allow_pickle=True)
+
+        elif mode == 'train':
+            data = np.load(FILE_PATH+'TEM_train_compressed.npy',allow_pickle=True)
+
+        elif mode == 'validation':
+            data = np.load(FILE_PATH+'TEM_validation_compressed.npy',allow_pickle=True)
+
+        else:
+            data = np.load(FILE_PATH+'TEM_test_compressed.npy',allow_pickle=True)
+        
+        self.transform = transform
+        
+        self.images=data[:,0]
+        self.labels=data[:,1]
+                
+    def __getitem__(self, index):
+        image, label = self.images[index], self.labels[index]
+        image = Image.fromarray(image)
+        if self.transform is not None:
+            image = self.transform(image)
+        return image, label
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def subset_maker(self,rate):
+        indexes=np.random.choice(len(self.images),int(len(self.images)*rate),replace=False)
+        images=self.images[indexes]
+        labels=self.labels[indexes]
+        self.images=images
+        self.labels=labels
+        return self
